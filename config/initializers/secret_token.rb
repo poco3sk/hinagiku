3b067009477a9b41380b3640f41e1059c22256be
krishna <skyblue_flare@yahoo.co.jp>
2013-02-15 19:31:33 +0900

    Init Project Hinagiku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hinagiku::Application.config.secret_token = '1ca759da250582fbac1aab4d25e156035a6491a93aee76dfb6ef26f6c44545faa4ac82ce68774565bb8ad24f6361efbac9946a229264c78a48158dc2494bce11'
