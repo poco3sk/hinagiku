77997eccec1b788823b87ec28cf5051216d1e051
krishna <skyblue_flare@yahoo.co.jp>
2013-02-18 22:17:08 +0900

    added. task update test

--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -98,4 +98,33 @@ describe "Tasks", type: :feature do
       end
     end
   end
+
+  describe "PUT UPDATE" do
+    before do
+      @task = Task.find(rand(5) + 1)
+      update_str = "UPDATE TEST"
+      @task.name += update_str
+      @task.due_date += 1.day
+      @task.description += update_str
+    end
+
+    it "update task page"  do
+      visit "/tasks/#{@task.id}/edit"
+
+      within("form") do |form|
+        fill_in '名称', with: @task.name
+        select @task.due_date.year, from: 'task_due_date_1i'
+        select @task.due_date.month, from: 'task_due_date_2i'
+        select @task.due_date.day, from: 'task_due_date_3i'
+        find("input#task_done[type=checkbox]").set(true)
+        fill_in '説明', with: @task.description
+      end
+
+      click_button '送信'
+
+      page.should have_content(@task.name)
+      page.should have_content(@task.description)
+      page.should have_content(@task.due_date.strftime("%Y-%m-%d"))
+    end
+  end
 end
