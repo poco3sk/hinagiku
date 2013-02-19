f562ff5f9b8227a7a542a19a6542486c617341c4
krishna <skyblue_flare@yahoo.co.jp>
2013-02-19 22:30:00 +0900

    modified. rquessts tasks
    部分的にcontroller testに

--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -6,31 +6,7 @@ describe "Tasks", type: :feature do
     5.times { create(:task) }
   end
 
-  describe "GET /" do
-    it "works!" do
-      visit root_path
-      page.status_code.should == 200
-      page.should have_content "タスクの一覧"
-    end
-
-    it "task list index" do
-      visit root_path
-      Task.all.each do |t|
-        page.should have_content t.name
-      end
-    end
-  end
-
-  describe "GET SHOW" do
-    it "show task" do
-      task = Task.last
-      visit "/tasks/#{task.id}"
-      find('#task_due_date').should have_content(task.due_date)
-      find('#task_description').should have_content(task.description)
-    end
-  end
-
-  describe "GET NEW" do
+  describe "new" do
     before do
       @year = 2013
       Timecop.freeze(Time.parse("#{@year}/2/17"))
@@ -39,13 +15,8 @@ describe "Tasks", type: :feature do
     it "task form" do
       visit new_task_path
 
-      within("form") do |form|
-        find("input#task_name[type=text]").should have_content("")
-        find("select#task_due_date_1i").should have_content([@year, @year + 1].join)
-        (1..3).each { |i| find("select#task_due_date_#{i}i").should be }
-        find("input#task_done[type=checkbox]").should_not be_checked
-        find("input[type=submit]").value.should == "送信"
-      end
+      find("select#task_due_date_1i").should have_content([@year, @year + 1].join)
+      find("input#task_done[type=checkbox]").should_not be_checked
     end
 
     after do
