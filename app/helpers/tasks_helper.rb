0754a8d9337701b41ebfc8afbbefa780e915930b
krishna <skyblue_flare@yahoo.co.jp>
2013-02-20 22:48:18 +0900

    added. task helper link_for

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,14 @@
+# -*- coding: utf-8 -*-
 module TasksHelper
+  def link_for(task)
+    links = [ link_to("修正", [ :edit, task ]) ]
+    if task.done?
+      links << link_to("戻す", [ :restart, task ], method: :put)
+    else
+      links << link_to("完了", [ :finish, task ], method: :put)
+    end
+
+    links << delete_link(task)
+    links.join(" ")
+  end
 end
