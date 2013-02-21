ca9f167778efdd5cd364cea1409a7637c112dba9
krishna <skyblue_flare@yahoo.co.jp>
2013-02-21 22:01:03 +0900

    added. task scope

--- app/models/task.rb
+++ app/models/task.rb
@@ -13,4 +13,7 @@
 
 class Task < ActiveRecord::Base
   attr_accessible :description, :done, :due_date, :name
+
+  scope :done, where(done: true).order(:due_date)
+  scope :undone, where(done: false).order("due_date desc")
 end
