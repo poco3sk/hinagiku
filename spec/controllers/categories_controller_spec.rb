3826c96281317cc0e46b91aad7e9bf1a115e610b
krishna <skyblue_flare@yahoo.co.jp>
2013-03-03 18:50:48 +0900

    modified. categories controller index test

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -1,12 +1,16 @@
 require 'spec_helper'
 
 describe CategoriesController do
+  before do
+    5.times { create(:category) }
+  end
 
   describe "GET 'index'" do
     it "returns http success" do
-      get 'index'
+      get :index
+
       response.should be_success
+      assigns(:categories).size.should == 5
     end
   end
-
 end
