02256c5aa7b7db4f7774233999b41ca03c9711d9
krishna <skyblue_flare@yahoo.co.jp>
2013-03-04 21:00:40 +0900

    added. category index link

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,8 @@
+# -*- coding: utf-8 -*-
 module CategoriesHelper
+  def link_for(category)
+    links = [ link_to("修正", [ :edit, category ]) ]
+    links << delete_link(category)
+    links.join(" ")
+  end
 end
