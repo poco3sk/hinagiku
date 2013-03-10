eae2ce41c020548b0e973a6ff2dba60e95681d05
krishna <skyblue_flare@yahoo.co.jp>
2013-03-10 20:24:55 +0900

    added. internal server error page
    p.118

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,17 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   clear_helpers
 
+  rescue_from Exception, with: :render_500
   rescue_from ActiveRecord::RecordNotFound, with: :render_404
 
   private
   def render_404(exception)
     raise exception if Rails.env.development?
     render "errors/not_found", status: 404
+  end
+
+  def render_500(exception)
+    raise exception if Rails.env.development?
+    render "errors/internal_server_error", status: 500
   end
 end
