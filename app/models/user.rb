6a06f533a252a39686a0305475f4ae172231bc68
krishna <skyblue_flare@yahoo.co.jp>
2013-03-14 22:59:37 +0900

    modified. User model auto_login_token
    (3) p.53

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
   attr_accessible :display_name, :login_name, :password, :password_confirmation
 
   has_secure_password
+
+  before_create do
+    self.auto_login_token = SecureRandom.hex
+  end
 end
