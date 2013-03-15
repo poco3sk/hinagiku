6587b7fe2839cfbff4618ec5ee91e1f5e3d574ad
krishna <skyblue_flare@yahoo.co.jp>
2013-03-15 22:36:24 +0900

    fixed. Session Controller new action skip authenticate
    (3) p.48

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  skip_before_filter :authenticate_user
+
   def create
     user = User.find_by_login_name(params[:login_name])
     if user && user.authenticate(params[:password])
